f9176247d12f9a715f666fe15079026e90ff3713
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2024-01-13 08:19:42 +0530

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6">
+          CyberDog
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 
---
+++ src/components/Rightbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Rightbar() {
+  return (
+    <div>Rightbar</div>
+  )
+}
+
+export default Rightbar
